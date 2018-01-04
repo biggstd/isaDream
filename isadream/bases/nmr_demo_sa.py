1a96f903c3df7aa3ffcb0ac8ad41571da66f7cc9
Tyler Biggs <biggstd@gmail.com>
2018-01-03 17:22:39 -0800

    progress in generating ISA handler funcs + demo doc

--- isadream/bases/nmr_demo_sa.py
+++ isadream/bases/nmr_demo_sa.py
@@ -6,7 +6,9 @@ NMR Demo Study and Assay Definitions
 
 This file defines the Study and Assay components
 necessary to generate a complete set of ISA *.json
-documents.
+document(s).
+
+This ISA file is for demo purposes.
 
 For each **Study** there must be:
 
@@ -48,11 +50,13 @@ data into a web browser.
 """
 
 from isatools.model import *
+from isatools.isajson import ISAJSONEncoder
+import json
 import pandas as pd
 
-
-pandas_df_comment = Comment()
-pandas_df_comment.name = ""
+# Import all of the custom ontologies defined
+# for the IDREAM project thus far.
+from ontologies import *
 
 
 def build_nmr_output():
@@ -71,56 +75,33 @@ def build_nmr_output():
         "data, experimental and simulation data, along with "
         "any associated metadata.")
 
-    nmr = OntologySource(
-        name='Nuclear Magnetic Resonance',
-        description="Concepts related to NMR experiments.")
+    # Ontology Sources --------------------------------------------------------
     inv.ontology_source_references.append(nmr)
-
-    intrinsic_properties = OntologySource(
-        name='Intrinsic Material Property',
-        description=(
-            'Propertiy of a material that are independent of '
-            'other context and other things.'))
     inv.ontology_source_references.append(intrinsic_properties)
-
-    extrinsic_properties = OntologySource(
-        name='Extrinsic Material Property',
-        description='Property of a material that is dependent '
-        'on context and relationships.')
     inv.ontology_source_references.append(extrinsic_properties)
 
-    ppm = OntologyAnnotation(term_source=nmr)
-    ppm.term = "ppm"
+    # Publications ------------------------------------------------------------
+    # TODO: Append publications to their corresponding studies.
+    sipos2006_talanta = Publication()
+    sipos2006_talanta.title = (
+        '27 Al NMR and Raman spectroscopic studies of alkaline aluminate '
+        'solutions with extremely high caustic content - Does the '
+        'octahedral species Al(OH)_6^{3-} exist in solution?')
+    sipos2006_talanta.doi = '10.1016/j.talanta.2006.02.008'
+    sipos2006_talanta.author_list = 'Pal Sipos, Glenn Hefter, Peter M. May'
 
-    al_27_nmr = OntologyAnnotation(term_source=nmr)
-    al_27_nmr.term = "27 Al NMR"
+    sipos2006_RSC = Publication()
+    sipos2006_RSC.title = (
+        'Chemical speciation in concentrated alkaline aluminate solutions '
+        'in sodium, potassium and caesium media. Interpretation of the '
+        'unusual variations of the observed hydroxide activity.')
+    sipos2006_RSC.doi = '10.1039/b513357b'
+    sipos2006_RSC.author_list = (
+        'Pal Sipos, Mark Schibeci, Gabor Peintler, Peter M. May, Glen Hefter')
 
-    molarity = OntologyAnnotation(term_source=amnt_conc)
-    molarity.term = "Molarity"
-
-    material_purity = OntologyAnnotation(term_source=extrinsic_properties)
-    material_purity.term = "Material Purity"
-
-    percent_material_purity = OntologyAnnotation(
-        term_source=extrinsic_properties)
-    percent_material_purity.term = 'Percent by Weight Purity'
-
-    degrees_celsius = OntologyAnnotation(term_source=extrinsic_properties)
-    degrees_celsius.term = "Temperature in degrees celsius"
-
-    # MATERIAL SOURCES
-
-    al_source = Source(name='Aluminum Wire')
-    al_source.characteristics.append(temp_characteristic)
-
-    sodium_hydroxide = Source(name='Sodium Hydroxide')
-
-    potassium_hydroxide = Source(name='Potassium Hydroxide')
-
-    lithium_hydroxide = Source(name='Lithium Hydroxide')
-
-    # STUDY FACTORS
-
+    # Study Factors -----------------------------------------------------------
+    # Study Factors are variables that an experimentalist changes in order to
+    # observe a result.
     molarity_factor = StudyFactor()
     molarity_factor.name = "Molarity Study Factor"
     molarity_factor.factor_type = molarity
@@ -129,48 +110,173 @@ def build_nmr_output():
     celsius.name = "Degrees Celsius"
     celsius.factor_type = degrees_celsius
 
-    # MATERIAL SAMPLES
-
+    # Material Samples --------------------------------------------------------
+    # Normally these would be unique on a per-study basis. For this demo case
+    # The same instances are used for simplicity.
     sodium_aluminate_soln = Sample()
     sodium_aluminate_soln.name = "The sodium solution used in sipos 2006."
     # Set the derives_from list to include all the source materials used.
-    sodium_aluminate_soln.derives_from = [al_source, sodium_hydroxide]
+    sodium_aluminate_soln.derives_from = [al_wire, sodium_hydroxide]
     sodium_aluminate_soln.factor_values = [
         FactorValue(factor_name=celsius, value=25, unit=degrees_celsius)
     ]
 
+    caesium_al_soln = Sample()
+    caesium_al_soln.name = 'Caesium Aluminate Solution'
+    caesium_al_soln.derives_from = [
+        al_wire,
+        caesium_hydroxide,
+        caesium_chloride,
+    ]
+
+    # Study definitions -------------------------------------------------------
     stu1 = Study()
-    stu1.identifier = "Sipos 2006 Study"
-    stu1.title = "Sipos 2006 NMR Study  Figure 2"
-    stu1.description = "Test study for Sipos' 2006 NMR study."
-    stu1.design_descriptors.append(ppm)
-    stu1.sources = [al_source, sodium_hydroxide]
+    stu1.identifier = "sipos2006_talanta"
+    stu1.title = "Sipos 2006 Talanta Study"
+    stu1.description = "Study abstraction of Sipos' 2006 Talanta paper."
+    stu1.publications = [sipos2006_talanta]
+    # Design descriptors define the type of study, ie. the primary
+    # measurements and conditions used.
+    stu1.design_descriptors = [
+        ppm,
+        al_27_nmr,
+        molarity
+    ]
+    stu1.sources = [
+        al_wire,
+        sodium_hydroxide,
+        potassium_hydroxide,
+        lithium_hydroxide
+    ]
     stu1.samples = [sodium_aluminate_soln]
     stu1.units = [ppm, molarity, celsius]
     stu1.factors = [molarity_factor, celsius]
     inv.studies.append(stu1)
 
-    # Table 1. from Sipos 2006 Dalton Trans.
     stu2 = Study()
-    stu2.identifier = ''
+    stu2.identifier = 'sipos2006_RSC'
+    stu2.title = 'Sipos 2006 Royal Society of Chemistry Study'
+    stu2.description = 'Study abstraction of Sipos 2006 RSC paper.'
+    stu2.publications = [sipos2006_RSC]
+    stu2.design_descriptors = [
+        ppm,
+        al_27_nmr,
+        molarity
+    ]
+    stu2.characteristic_categoires = [
+        aluminum,
+        hydroxide,
+        caesium
+    ]
+    stu2.sources = [
+        al_wire,
+        sodium_hydroxide,
+        caesium_hydroxide,
+        caesium_chloride
+    ]
+    inv.studies.append(stu2)
 
-    # Sipos 2006 Science Direct.
-    # Build a protocol, or add factor values to track the following.
-    # 
-    # 
+    # Assay definitions -------------------------------------------------------
 
-    assay = Assay()
-    assay.measurement_type = ppm
-    assay.technology_type = al_27_nmr
-    assay.technology_platform = "Bruker DPX 300MHz"
-    assay.samples = [sodium_aluminate_soln]
-    assay.units = [ppm, molarity, celsius]
-    assay.data_files = [DataFile(filename='sipos2006-fig2.csv')]
-    assay.sources = [al_source, sodium_hydroxide]
+    sipos_2006_talanta_fig_3 = Assay()
+    sipos_2006_talanta_fig_3.measurement_type = ppm
+    sipos_2006_talanta_fig_3.technology_type = al_27_nmr
+    sipos_2006_talanta_fig_3.technology_platform = 'Bruker 300Mhz'
+    sipos_2006_talanta_fig_3.sources = [
+        al_wire,
+        potassium_hydroxide,
+        sodium_hydroxide,
+        lithium_hydroxide,
+    ]
+    sipos_2006_talanta_fig_3.samples = [
+        
+    ]
 
-    stu.assays.append(assay)
+    sipos_2006_RSC_table1 = Assay()
+    sipos_2006_RSC_table1.measurement_type = ppm
+    sipos_2006_RSC_table1.technology_type = al_27_nmr
+    sipos_2006_RSC_table1.technology_platform = 'Bruker 300MHz'
+    sipos_2006_RSC_table1.sources = [
+        al_wire,
+        caesium_chloride,
+        caesium_hydroxide
+    ]
+    sipos_2006_RSC_table1.samples = [caesium_al_soln]
+    sipos_2006_RSC_table1.units = [ppm, molarity, celsius]
+    sipos_2006_RSC_table1.data_files = [
+        DataFile(
+            filename='sipos2006_RSC_table_1.csv',
+            comments=[
+                # An ad-hoc way to track the column headers and relate
+                # them to their associated Ontology annotations.
+                Comment(name='column_0', value='molarity aluminum'),
+                Comment(name='column_1', value='molarity hydroxide'),
+                Comment(name='column_2', value='ppm aluminum')
+            ]
+        )
+    ]
+    stu2.assays.append(sipos_2006_RSC_table1)
+
+    # assay = Assay()
+    # assay.measurement_type = ppm
+    # assay.technology_type = al_27_nmr
+    # assay.technology_platform = "Bruker DPX 300MHz"
+    # assay.samples = [sodium_aluminate_soln]
+    # assay.units = [ppm, molarity, celsius]
+    # assay.data_files = [DataFile(filename='sipos2006-fig2.csv')]
+    # assay.sources = [al_wire, sodium_hydroxide]
+
+    # stu1.assays.append(assay)
 
     return inv
+
+
+def get_studies_by_design_descriptor(investigation, descriptor):
+    """Returns ISA Study objects with the given descriptor.
+
+    Searches an ISA Investigation object and returns Study
+    objects that have the given descriptor string.
+    """
+
+    # Create an empty list to store the studies to be returned.
+    matching_study_list = list()
+
+    # Pull all the studies from a given investigation, and store
+    # them in a list. This is an ISA tools property call that
+    # returns a list of studies.
+    all_studies_list = investigation.studies
+
+    # Seach each of these studies for the desired descriptor.
+    for study in all_studies_list:
+        # If the given descriptor is found within, append this
+        # study to the output list.
+        if descriptor in study.design_descriptors:
+            matching_study_list.append(study)
+
+    return matching_study_list
+
+
+def get_assays_by_measurement_type(study, measurement_ontology):
+    """Returns ISA Assay objects with the given measurement type.
+
+    Searches an ISA Study object, and returns a list of ISA assay
+    objects with the matching measurement ontology.
+    """
+
+    # Create an empty list to store the Assay objects to be returned.
+    matching_assay_list = list()
+
+    # Pull all of the all of the Assay objects from the input Study.
+    all_assays_list = study.assays
+
+    # Search each assay, and append it to the output list if the
+    # measurement type matches the input given in
+    # measurement_ontology.
+    for assay in all_assays_list:
+        if measurement_ontology == assay.measurement_type:
+            matching_assay_list.append(assay)
+
+    return matching_assay_list
 
 
 def get_dataFiles_from_assay(assay):
@@ -194,44 +300,88 @@ def get_dataFiles_from_assay(assay):
     return data_files
 
 
-def build_metadata_dict(isa_investigation, key):
-    """
-    Builds a dictionary from an isa_investigation object.
-    Essentially builds a dictionary from...
-
-    # TODO: What level of the investigation, study, assay should
-    this key be assigned to?
-
-    :param isa_investigation:
-        An ISA Investigation object to be processed.
-
-    :param key:
-        The key to assign to the given ISA investigation.
-
-    :returns:
-        {key: isa_investigation} dictionary.
-    """
-    pass
-
-
-def build_pandas_dataframe(dataFile):
+def build_pandas_dataframe(data_file):
     """
     Reads a custom comment in the dataFile object input.
 
+    TODO: Add error handling as decribed below.
     THIS FUNCTION SHOULD READ THE CUSTOM COMMENT EMBEDDED
     WITHIN THE DATAFILE OBJECT. IT SHOULD THROW AN ERROR
     IF THIS COMMENT IS NOT PRESENT.
     """
+
     # Get the data file path.
+    data_path = data_file.filename
+
+    # TODO: Implement below...
+    # Get the pandas csv options from the comment.
+    # Create the dataframe with the options loaded.
+
+    # Create the dataframe.
+    data_frame = pd.read_csv(
+        filepath_or_buffer=data_path,
+        index_col=False
+    )
 
     # Get the column headers from the comment.
     # Get the comment by name using the ISA tools function.
     # get_comment(self, name)
+    # Find the number of columns in the data frame, then
+    # iterate through the column_# list to this length to
+    # retrieve the proper column names.
+    new_column_names = list()
 
-    # Get the pandas csv options from the comment.
+    for i in range(len(data_frame.columns)):
+        # Build the comment name that maps to column_i.
+        i_col_name = data_file.get_comment('column_{}'.format(i))
+        # Add that comment valuye to the list of new column names.
+        new_column_names.append(i_col_name)
 
-    # Create the dataframe with the options loaded.
+    # Assign the new column names to the data frame.
+    data_frame.columns = new_column_names
 
-    # df = pd.read_csv
+    return data_frame
 
-    return dataframe
+
+def add_ISA_metadata_key_to_pandas_dataframe(data_frame, col, val):
+    """Adds the study and assay identifier to an input data_frame.
+
+    This simply creates a new column `col` the dataframe, and assigns
+    all values of that column to the input given in `val`.
+
+    The purpose of this is so that an extracted and processed
+    data set from a given data file can be associated with the
+    correct set of metadata.
+    """
+    data_frame[col] = val
+
+    # TODO: Unclear if I need to return this value... I think the
+    # code above should modify the dataframe in place...
+
+    return data_frame
+
+
+def jsonify_investigation(investigation):
+    """Converts an ISA Investigation object to a .json string.
+    """
+    return json.dumps(
+        investigation,
+        cls=ISAJSONEncoder,
+        sort_keys=True,
+        indent=4,
+        separators=(',', ': ')
+    )
+
+
+if __name__ == '__main__':
+    invest = build_nmr_output()
+    print(jsonify_investigation(invest))
+    # Testing some other functions.
+    matching_studies = get_studies_by_design_descriptor(invest, al_27_nmr)
+
+    out_assays = list()
+
+    for study in matching_studies:
+        matching_assays = get_assays_by_measurement_type(study, ppm)
+        out_assays.extend(matching_assays)
+    print(out_assays)
--- isadream/bases/ontologies.py
+++ isadream/bases/ontologies.py
@@ -7,17 +7,12 @@ Contains all the ontology definitions used in the IDREAM
 database project.
 """
 
-from isatools.model import (
-    OntologySource,
-    OntologyAnnotation,
-    Source,
-)
+from isatools.model import *
 
 # --------------------ONTOLOGY SOURCES--------------------#
 # These are the 'top-level' definitions.
 
-nmr = OntologySource()
-nmr.name = 'Nuclear Magnetic Resonance'
+nmr = OntologySource(name='Nuclear Magnetic Resonance')
 nmr.file = 'http://goldbook.iupac.org/html/C/C01036.html'
 nmr.description = (
     'The fractional variation of the resonance frequency of a '
@@ -25,8 +20,7 @@ nmr.description = (
     'in consequence of its magnetic environment.'
 )
 
-amount_conc = OntologySource()
-amount_conc.name = 'Amount Concentration'
+amount_conc = OntologySource(name='Amount Concentration')
 amount_conc.file = 'https://goldbook.iupac.org/html/A/A00295.html'
 amount_conc.description = (
     'Amount of a constituent divided by the volume of the mixture. '
@@ -37,30 +31,57 @@ amount_conc.description = (
     'litre(mol L −1) sometimes denoted by M.'
 )
 
-intrinsic_prop = OntologySource()
+intrinsic_properties = OntologySource(
+    name='Intrinsic Material Property',
+    description=(
+        'Propertiy of a material that are independent of '
+        'other context and other things.'))
 
-extrinsic_prop = OntologySource()
+extrinsic_properties = OntologySource(
+    name='Extrinsic Material Property',
+    description='Property of a material that is dependent '
+    'on context and relationships.')
 
 
 # --------------------ONTOLOGY ANNOTATIONS---------------- #
 # These are the 'mid-level' definitions.
 
-ppm = OntologyAnnotation()
+ppm = OntologyAnnotation(term_source=nmr)
+ppm.term = "ppm"
 
-Al_27_nmr = OntologyAnnotation()
+al_27_nmr = OntologyAnnotation(term_source=nmr)
+al_27_nmr.term = "27 Al NMR"
 
-molarity = OntologyAnnotation()
+molarity = OntologyAnnotation(term_source=extrinsic_properties)
+molarity.term = "Molarity"
 
-material_purity = OntologyAnnotation()
+material_purity = OntologyAnnotation(term_source=extrinsic_properties)
+material_purity.term = "Material Purity"
+
+percent_material_purity = OntologyAnnotation(
+    term_source=extrinsic_properties)
+percent_material_purity.term = 'Percent by Weight Purity'
+
+degrees_celsius = OntologyAnnotation(term_source=extrinsic_properties)
+degrees_celsius.term = "Temperature in degrees celsius"
+
+# Define ontology annotations for use in drop-downs.
+# These definitions should allow for...
+# TOOD: Can these be more directly linked to samples?
+hydroxide = OntologyAnnotation(term='Hydroxide')
+aluminum = OntologyAnnotation(term='Aluminum')
+sodium = OntologyAnnotation(term='Sodium')
+caesium = OntologyAnnotation(term='Caesium')
 
 
 # --------------------MATERIAL SOURCES----------------#
 # These are the 'top-level' definitions of materials.
 
-Al_metal = Source()
+al_wire = Source()
+al_wire.name = 'Aluminum Wire'
 
-NaOH = Source()
-
-KOH = Source()
-
-LiOH = Source()
+sodium_hydroxide = Source(name='Sodium Hydroxide')
+potassium_hydroxide = Source(name='Potassium Hydroxide')
+lithium_hydroxide = Source(name='Lithium Hydroxide')
+caesium_hydroxide = Source(name='Caesium Hydroxide')
+caesium_chloride = Source(name='Caesium Chloride')
