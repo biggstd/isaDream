fd818798a24d496be4fd116daa8d12d40c1e117d
Tyler Biggs <biggstd@gmail.com>
2018-05-23 11:29:46 -0700

    Added http prefix to the prefix option.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 bokeh serve --port ${PORT}\
-  --prefix idreamvisualization.pnl.gov
+  --prefix http://idreamvisualization.pnl.gov
   --address 0.0.0.0\
   --allow-websocket-origin ${ORIGIN}\
   --log-level ${LOG_LEVEL}\
