17b30f44922a7343604916bde1646e7262da1fc4
Tyler Biggs <biggstd@gmail.com>
2018-05-23 10:56:13 -0700

    Added X-headers and changed the bokeh serve prefix.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 bokeh serve --port ${PORT}\
+  -use-xheaders
+  --prefix http://idreamvisualization.pnl.gov
   --address 0.0.0.0\
   --allow-websocket-origin ${ORIGIN}\
   --log-level ${LOG_LEVEL}\
