417fb254a05acbc9df3e75f8b96eaee153c4ad7f
Michael Akopov <makopov@pnnl.gov>
2018-05-23 22:11:16 -0700

    Updating entrypoint address for docker container
        modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ bokeh serve\
   --use-xheaders\
   --address 0.0.0.0\
   --allow-websocket-origin 0.0.0.0\
-  --allow-websocket-origin localhost\
-  --allow-websocket-origin idreamvisualization.pnl.gov\
+  --allow-websocket-origin localhost:8123\
+  --allow-websocket-origin idreamvisualization.pnl.gov:443\
   --log-level debug\
   /bokehtest\
   /testvis\
