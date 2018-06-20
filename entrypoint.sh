4ae787befd0061c87c6bbf6bd1360142c47a3b16
Tyler Biggs <biggstd@gmail.com>
2018-06-20 09:40:12 -0700

    Lowered log level of the bokeh server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,5 @@ bokeh serve\
   --allow-websocket-origin localhost:8123\
   --allow-websocket-origin idreamvisualization.pnl.gov\
   --allow-websocket-origin lampdev02.pnl.gov:8123\
-  --log-level debug\
   /bokehtest\
   /testvis\
