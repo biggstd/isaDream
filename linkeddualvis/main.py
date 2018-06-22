8bb043cf6be2abc008be7de1fde9cc17d9e8d449
Tyler Biggs <biggstd@gmail.com>
2018-06-22 16:24:59 -0700

    New json schema version.

---
+++ linkeddualvis/main.py
@@ -0,0 +1,20 @@
+'''
+
+Generic Linked Plots
+====================
+
+Author: Tyler Biggs
+
+'''
+
+# General imports.
+
+
+# Bokeh imports.
+import bokeh
+import bokeh.io
+import bokeh.layouts
+import bokeh.plotting
+
+
+# Gather the data.
