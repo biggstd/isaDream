7d2537f5cd7b254ca50c059f9c8b3bda626294a6
Tyler Biggs <biggstd@gmail.com>
2018-06-28 16:38:25 -0700

    Updated entrypoint, remoted port from new ip.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ bokeh serve\
   --port 5006\
   --use-xheaders\
   --address 0.0.0.0\
-  --allow-websocket-origin 0.0.0.0\
-  --allow-websocket-origin=130.20.47.245:8123\
+  --allow-websocket-origin=130.20.47.245\
   --allow-websocket-origin localhost:8123\
   --allow-websocket-origin idreamvisualization.pnl.gov\
   --allow-websocket-origin lampdev02.pnl.gov:8123\
